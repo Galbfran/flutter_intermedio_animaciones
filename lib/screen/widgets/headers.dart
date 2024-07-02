08ceeef49a031d27fd682d0c43b8cb90dd8033de
franco galbiati <francogalbiati984@gmail.com>
2024-07-02 20:16:33 -0300

    add header gradiente

--- lib/screen/widgets/headers.dart
+++ lib/screen/widgets/headers.dart
@@ -214,6 +214,67 @@ class HeaderWaves extends StatelessWidget {
 class _HeaderWavesPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
+    final Rect rect =
+        Rect.fromCircle(center: const Offset(135.0, 55.0), radius: 180);
+
+    const Gradient gradiente = LinearGradient(
+      colors: [
+        Color(0xffC012FF),
+        Color(0xff6D05E8),
+      ],
+      begin: Alignment.topCenter,
+    );
+
+    final paint = Paint()..shader = gradiente.createShader(rect);
+    // propiedades
+
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 5;
+
+    final path = Path();
+
+    path.lineTo(0, size.height * 0.25);
+    path.quadraticBezierTo(
+      size.width * 0.25,
+      size.height * 0.15,
+      size.width * 0.5,
+      size.height * 0.25,
+    );
+    path.quadraticBezierTo(
+      size.width * 0.75,
+      size.height * 0.35,
+      size.width,
+      size.height * 0.25,
+    );
+    path.lineTo(size.width, 0);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class HeaderGradiente extends StatelessWidget {
+  const HeaderGradiente({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Colors.blue,
+      child: CustomPaint(
+        painter: _HeaderGradientePainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderGradientePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
     final paint = Paint();
     // propiedades
     paint.color = Colors.red;
